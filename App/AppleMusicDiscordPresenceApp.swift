1c687c4d2952d7ce5d5c034e21b1d36cf12d08c3
tnoa4842 <tnoa4842@gmail.com>
2026-08-11 01:06:21 +0900

    Update AppleMusicDiscordPresenceApp.swift

--- App/AppleMusicDiscordPresenceApp.swift
+++ App/AppleMusicDiscordPresenceApp.swift
@@ -2,12 +2,15 @@ import SwiftUI
 
 @main
 struct AppleMusicDiscordPresenceApp: App {
+
     @StateObject private var model = PresenceViewModel()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environmentObject(model)
+            ContentView(model: model)
+                .task {
+                    await model.start()
+                }
         }
     }
 }
